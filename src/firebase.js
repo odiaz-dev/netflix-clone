54c5b51e143df9cc7e810da6d0d913f288ad39e8
Oscar Diaz | 🚀 <oscardiazaed@gmail.com>
2023-08-24 10:22:15 -0400

    inicio de clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB2Ob1CC0gqnWSnv8bdt7gu7IBTXPN2zEg',
+  authDomain: 'netflix-clone-fb2b7.firebaseapp.com',
+  projectId: 'netflix-clone-fb2b7',
+  storageBucket: 'netflix-clone-fb2b7.appspot.com',
+  messagingSenderId: '972489202859',
+  appId: '1:972489202859:web:e51a470b0de0479f606bee',
+};
